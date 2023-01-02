81d3b26b605029ec05a5c794eeffff64bee67ac0
fer <fernando@backbase.com>
2023-01-02 10:09:42 +0000

    .

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup.py
 from setuptools import setup
 
 setup(
-    name='ghgqlclient',
+    name='github-graphql-client',
     version='0.1',
     description='Module for consuming the Github GraphQL API.',
     author='fer',
     author_email='fer@ferqwerty.com',
-    packages=['ghgqlclient'],
+    packages=['github-graphql-client'],
     install_requires=[
         'numpy',
         'pandas',
